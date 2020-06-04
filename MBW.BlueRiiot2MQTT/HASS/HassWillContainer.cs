f8271acd494dd06e06ba5462eaa5070204c55fba
Michael Bisbjerg <michael@mbwarez.dk>
2020-06-05 00:59:19 +0200

    Include software version in status sensor

--- MBW.BlueRiiot2MQTT/HASS/HassWillContainer.cs
+++ MBW.BlueRiiot2MQTT/HASS/HassWillContainer.cs
@@ -7,7 +7,7 @@ namespace MBW.BlueRiiot2MQTT.HASS
     internal class HassWillContainer
     {
         private readonly HassMqttSensor _sensor;
-        
+
         // "problem" device class: on = problem; off = no problem
         public const string RunningMessage = "OFF";
         public const string NotRunningMessage = "ON";
@@ -16,10 +16,13 @@ namespace MBW.BlueRiiot2MQTT.HASS
 
         public HassWillContainer(SensorStore sensorStore)
         {
+            var version = typeof(Program).Assembly.GetName().Version.ToString(3);
+
             _sensor = sensorStore.Create("BlueRiiot2MQTT Status", "blueriiot2mqtt_status", HassDeviceType.BinarySensor,
                     "blueriiot2mqtt", "status", HassDeviceClass.Problem)
                 .SetDeviceProperty(HassMqttSensorDeviceProperty.Name, "BlueRiiot2MQTT")
-                .SetDeviceProperty(HassMqttSensorDeviceProperty.SoftwareVersion,  typeof(Program).Assembly.GetName().Version.ToString(3))
+                .SetDeviceProperty(HassMqttSensorDeviceProperty.SoftwareVersion, version)
+                .SetAttribute("version", version)
                 .SetAttribute("started", DateTime.UtcNow)
                 .AddDeviceIdentifier("blueriiot2mqtt");
         }
